f70ab512283c1e8927fffe985c2381661b5306a9
pajjeee <127712783+pajjeee@users.noreply.github.com>
2023-09-24 17:52:23 +0700

    Add files via upload

---
+++ tugas3.2/AplikasiLatihanTiga3/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace AplikasiLatihanTiga3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.txtPesan1 = new System.Windows.Forms.TextBox();
+            this.txtPesan2 = new System.Windows.Forms.TextBox();
+            this.txtPesan3 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lstPekerjaan = new System.Windows.Forms.ListBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(76, 83);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nama";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(76, 152);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Pendidikan";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(76, 220);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Pekerjaan";
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(328, 83);
+            this.txtNama.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(316, 31);
+            this.txtNama.TabIndex = 3;
+            this.txtNama.TextChanged += new System.EventHandler(this.txtNama_TextChanged);
+            // 
+            // txtPesan1
+            // 
+            this.txtPesan1.Location = new System.Drawing.Point(150, 370);
+            this.txtPesan1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtPesan1.Name = "txtPesan1";
+            this.txtPesan1.Size = new System.Drawing.Size(494, 31);
+            this.txtPesan1.TabIndex = 4;
+            // 
+            // txtPesan2
+            // 
+            this.txtPesan2.Location = new System.Drawing.Point(150, 414);
+            this.txtPesan2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtPesan2.Name = "txtPesan2";
+            this.txtPesan2.Size = new System.Drawing.Size(494, 31);
+            this.txtPesan2.TabIndex = 5;
+            // 
+            // txtPesan3
+            // 
+            this.txtPesan3.Location = new System.Drawing.Point(150, 472);
+            this.txtPesan3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtPesan3.Name = "txtPesan3";
+            this.txtPesan3.Size = new System.Drawing.Size(494, 31);
+            this.txtPesan3.TabIndex = 6;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "SD",
+            "SMP",
+            "SMA",
+            "Diploma",
+            "Sarjana",
+            "Pascasrajana"});
+            this.comboBox1.Location = new System.Drawing.Point(328, 152);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(316, 33);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // lstPekerjaan
+            // 
+            this.lstPekerjaan.FormattingEnabled = true;
+            this.lstPekerjaan.ItemHeight = 25;
+            this.lstPekerjaan.Items.AddRange(new object[] {
+            "Pedagang",
+            "Pns",
+            "Dosen",
+            "Swasta",
+            "ABRI"});
+            this.lstPekerjaan.Location = new System.Drawing.Point(328, 219);
+            this.lstPekerjaan.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.lstPekerjaan.Name = "lstPekerjaan";
+            this.lstPekerjaan.Size = new System.Drawing.Size(316, 129);
+            this.lstPekerjaan.TabIndex = 8;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(150, 533);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(496, 36);
+            this.btnOK.TabIndex = 9;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 703);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.lstPekerjaan);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtPesan3);
+            this.Controls.Add(this.txtPesan2);
+            this.Controls.Add(this.txtPesan1);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.TextBox txtPesan1;
+        private System.Windows.Forms.TextBox txtPesan2;
+        private System.Windows.Forms.TextBox txtPesan3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox lstPekerjaan;
+        private System.Windows.Forms.Button btnOK;
+    }
+}
+
---
+++ tugas3.2/AplikasiLatihanTiga3/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AplikasiLatihanTiga3
+{
+    public partial class Form1 : Form
+    {
+        private object cmbPendidikan;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            var nama = txtNama.Text;
+            var pendidikan = comboBox1.Text;
+            var pekerjaan = lstPekerjaan.Text;
+
+            txtPesan1.Text = string.Format("hallo {0}", nama);
+            txtPesan2.Text = string.Format("pendidikan anda {0} ya?", pendidikan);
+            txtPesan3.Text = string.Format("pendidikan anda {0} ya?", pekerjaan);
+        }
+
+        private void txtNama_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
